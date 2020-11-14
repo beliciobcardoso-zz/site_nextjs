0455c64e20208056e151d01b91dc683c50e5e4cf
Belicio Cardoso <belloinfo@gmail.com>
2020-11-14 07:08:05 -0300

    primeira pagina com next

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+function Home() {
+    return (<div>
+        <h1>Pagina do Contador</h1>
+        <Contador />
+    </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+
+export default Home;
