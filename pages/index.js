67af29e4ecc4b7e67894aacda2edfdf8649e58e0
Belicio Cardoso <belloinfo@gmail.com>
2020-11-14 07:21:04 -0300

    Nova pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,10 @@
-import { useState } from 'react';
-
 function Home() {
     return (<div>
-        <h1>Pagina do Contador</h1>
-        <Contador />
+        <h1>Pagina do Principak</h1>
     </div>
     )
 }
 
-function Contador() {
-    const [contador, setContador] = useState(1);
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
+
 
 export default Home;
